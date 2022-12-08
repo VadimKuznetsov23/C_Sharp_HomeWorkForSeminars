b4c017c03297e451b4707db0e23ca7a25499c4c5
Vadim <v.v.kuznetsov@list.ru>
2022-12-08 23:04:01 +0300

    task10 done

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое число: ");
+int num = int.Parse(Console.ReadLine()!);
+if(num > 99 && num < 1000)
+{
+Console.WriteLine(num = num%10);
+
+if(num>999 && num<10000)
+
+    Console.WriteLine(num = num/10%10);
+}
+
+
+

9e0667d93179c0125865c150fcbdf55d1c136ead
Vadim <v.v.kuznetsov@list.ru>
2022-12-09 22:55:30 +0300

    task19 done

--- HomeWork/Seminar3/task19/Program.cs
+++ HomeWork/Seminar3/task19/Program.cs
@@ -2,3 +2,26 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+int i = num/10000;
+int j = num%10;
+int k = num/1000%10;
+int n = num/10%10;
+if (num > 9999 && num < 100000)
+{
+    if (i==j && k==n)
+    {
+    Console.WriteLine("Это число палиндром");
+    }
+        else 
+        {
+            Console.WriteLine("Это число не палиндром");
+        }
+}
+else 
+{
+    Console.WriteLine("Это не пятизначное число");
+}
+

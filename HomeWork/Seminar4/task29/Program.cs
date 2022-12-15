f03a342ff11a5b819d5cc3e5938ebea46373de2f
Vadim <v.v.kuznetsov@list.ru>
2022-12-15 21:01:49 +0300

    task29 done

--- HomeWork/Seminar4/task29/Program.cs
+++ HomeWork/Seminar4/task29/Program.cs
@@ -16,7 +16,7 @@ void PrintArray(int[] arr)
     Console.Write("[");
     for(int i = 0; i < arr.Length; i++)
     {
-        if(i < arr.Length - 1) Console.Write($"{arr[i]},");           
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");           
         else Console.Write($"{arr[i]}");
     }
      Console.Write("]");

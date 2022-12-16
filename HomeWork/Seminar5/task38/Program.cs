338065a9a357f8b9c4da975c177b82729c542c0b
Vadim <v.v.kuznetsov@list.ru>
2022-12-16 18:04:07 +0300

    task36 done

--- HomeWork/Seminar5/task38/Program.cs
+++ HomeWork/Seminar5/task38/Program.cs
@@ -1,3 +1,27 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+int[] newArray = GetArray(10, 1, 99);
+PrintArrey(newArray);
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива равна: {newArray}");
+int[] GetArray (int size, int minNum, int maxNum)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(minNum, maxNum + 1);
+    }
+    return arr;
+}
+void PrinArray(int[] newarr)
+{
+    Console.Write("[");
+    for(int i = 0; i < newarr.Length; i++)
+    {
+        if (i < newarr.Length - 1)
+        {
+            Console.Write($"{newarr[i]}, ");
+        }
+        else Console.Write($"{newarr[i]}]");
+    }
+}

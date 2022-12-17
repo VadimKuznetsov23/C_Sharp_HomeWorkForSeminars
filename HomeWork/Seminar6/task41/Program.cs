663db454b4dd162f11d65cc5083ed104c10a6491
Vadim <v.v.kuznetsov@list.ru>
2022-12-17 17:35:13 +0300

    task43 done

---
+++ HomeWork/Seminar6/task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string ReadLine(string message)
+{Console.WriteLine(message);
+return Console.ReadLine()!;
+}
+int[] GetNum(string message)
+{
+    int[] arr = new int[message.Length];
+    for(int i = 0; i < message.Length; i++)
+    {
+        arr[i] = message[0];
+    }
+    return arr;
+}
+void PrintArray(int[] res)
+{
+    for(int i = 0; i < res.Length; i++)
+    {
+        Console.Write($"{res[i]} ");
+    }
+}
+string numbers = ReadLine("Введите любое число");
+// int[] neArr = GetNum(numbers);
+// PrintArray(neArr);
+Console.Write(numbers[1]);
---
+++ HomeWork/Seminar6/task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double ReadLine(string numName)
+{
+    Console.Write($"Введите заначение {numName}: ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+void GetDotCross(double z1, double q1, double z2, double q2)
+{//x в уравнениях y = k1 * x + b1, y = k2 * x + b2; ищем системой из этих уравнений: 
+//y-y=(k1*x+b1)-(k2*x+b2) = (k1-k2)*x-(-b1+b2);
+//(k1-k2)*x-(-b1+b2)=0; 
+//(k1-k2)*x=(-b1+b2); 
+//x=(-b1+b2)/(k1-k2);
+    double x = (-z1+z2)/(q1-q2);
+    double crosCor1 = q1*x+z1;
+    double crosCor2 = q2*x+z2;
+    Console.Write($"Точка пересечения прямых имеет координаты:({crosCor1}; {crosCor2})");
+}
+
+double b1 = ReadLine("b1");
+double k1 = ReadLine("k1");
+double b2 = ReadLine("b2");
+double k2 = ReadLine("k2");
+GetDotCross(b1, k1, b2, k2);
+ 
+

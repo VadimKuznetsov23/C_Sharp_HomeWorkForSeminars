558b2292c33056f4e5130ebfeb6cfc8b976a9d80
Vadim <v.v.kuznetsov@list.ru>
2022-12-17 18:01:23 +0300

    task43 done

--- HomeWork/Seminar6/task43/Program.cs
+++ HomeWork/Seminar6/task43/Program.cs
@@ -3,27 +3,29 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-double ReadLine(string numName)
+//x в уравнениях y = k1 * x + b1, y = k2 * x + b2; будем искать системой уравнений: 
+//y-y=k1*x+b1-(k2*x+b2) = k1*x-k2*x-(-b1+b2) = (k1-k2)*x-(-b1+b2);
+//(k1-k2)*x-(-b1+b2)=0; 
+//(k1-k2)*x=(-b1+b2); 
+//x=(-b1+b2)/(k1-k2);
+
+double ReadInput(string numName)
 {
     Console.Write($"Введите заначение {numName}: ");
     return Convert.ToDouble(Console.ReadLine());
 }
-void GetDotCross(double z1, double q1, double z2, double q2)
-{//x в уравнениях y = k1 * x + b1, y = k2 * x + b2; ищем системой из этих уравнений: 
-//y-y=(k1*x+b1)-(k2*x+b2) = (k1-k2)*x-(-b1+b2);
-//(k1-k2)*x-(-b1+b2)=0; 
-//(k1-k2)*x=(-b1+b2); 
-//x=(-b1+b2)/(k1-k2);
-    double x = (-z1+z2)/(q1-q2);
-    double crosCor1 = q1*x+z1;
-    double crosCor2 = q2*x+z2;
+void GetDotCross(double q1, double z1, double q2, double z2) // Метод находит х и выводит точку пересечения.
+{
+    double x = (-q1+q2)/(z1-z2);
+    double crosCor1 = z1*x+q1;
+    double crosCor2 = z2*x+q2;
     Console.Write($"Точка пересечения прямых имеет координаты:({crosCor1}; {crosCor2})");
 }
 
-double b1 = ReadLine("b1");
-double k1 = ReadLine("k1");
-double b2 = ReadLine("b2");
-double k2 = ReadLine("k2");
+double b1 = ReadInput("b1");
+double k1 = ReadInput("k1");
+double b2 = ReadInput("b2");
+double k2 = ReadInput("k2");
 GetDotCross(b1, k1, b2, k2);
  
 

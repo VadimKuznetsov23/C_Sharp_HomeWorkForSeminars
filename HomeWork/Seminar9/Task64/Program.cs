49ade0f504a7ec06ee647e55fe88f0e0dc2459f2
Vadim <v.v.kuznetsov@list.ru>
2023-01-27 09:03:04 +0300

    fixed

--- HomeWork/Seminar9/Task64/Program.cs
+++ HomeWork/Seminar9/Task64/Program.cs
@@ -5,7 +5,7 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 
 Console.WriteLine("Введите натуральное число больше 1:");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()!);
 void NumberCounter (int number)
 {
     if (number < 0) Console.Write($"{number} не натуральное число");

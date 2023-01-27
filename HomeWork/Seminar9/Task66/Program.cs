0cc7cb41f567f3f2a55508b80dfb998788f53346
Vadim <v.v.kuznetsov@list.ru>
2023-01-27 08:59:22 +0300

    fixed

--- HomeWork/Seminar9/Task66/Program.cs
+++ HomeWork/Seminar9/Task66/Program.cs
@@ -7,7 +7,7 @@ M = 4; N = 8. -> 30
 Console.WriteLine("Введите начальное число M:");
 int numberM = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите начальное число M:");
+Console.WriteLine("Введите начальное число N:");
 int numberN = int.Parse(Console.ReadLine());
 
 void GetNumberSum (int numberM, int numberN, int sum)
